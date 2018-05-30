f9a4587cc97c01c17a72c1609b49673f771ba08d
Anwesh43 <anweshthecool0@gmail.com>
2018-05-31 01:14:33 +0530

    adding StateNode class

--- linecirclerotatorview/src/main/java/com/anwesh/uiprojects/linecirclerotatorview/LineCircleRotatorView.kt
+++ linecirclerotatorview/src/main/java/com/anwesh/uiprojects/linecirclerotatorview/LineCircleRotatorView.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
 
+val STATE_NODES : Int = 3
+
 class LineCircleRotatorView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -26,7 +28,22 @@ class LineCircleRotatorView(ctx : Context) : View(ctx) {
         return true
     }
 
-    data class State (var dir : Float = 0f, var scale : Float = 0f, var prevScale : Float = 0f) {
+    data class StateNode (var i : Int, var dir : Float = 0f, var scale : Float = 0f, var prevScale : Float = 0f) {
+
+        private var next : StateNode? = null
+
+        private var prev : StateNode? = null
+
+        init {
+            this.addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < STATE_NODES - 1) {
+                next = StateNode(i + 1)
+                next?.prev = this
+            }
+        }
 
         fun update(stopcb : (Float) -> Unit) {
             scale += 0.1f * dir
@@ -43,6 +60,18 @@ class LineCircleRotatorView(ctx : Context) : View(ctx) {
                 dir = 1 - 2 * prevScale
                 startcb()
             }
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : StateNode {
+            var curr : StateNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 
