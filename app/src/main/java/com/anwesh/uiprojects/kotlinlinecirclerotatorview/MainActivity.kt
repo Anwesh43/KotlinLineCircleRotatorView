fa02216440bc49b6c1fbbc34ddb88d13cd766a70
Anwesh43 <anweshthecool0@gmail.com>
2018-05-31 02:13:39 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinecirclerotatorview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinecirclerotatorview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinecirclerotatorview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linecirclerotatorview.LineCircleRotatorView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        LineCircleRotatorView.create(this)
     }
 }
